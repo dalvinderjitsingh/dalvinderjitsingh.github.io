cd2e247727cc0fe6cd60eb827befb57d7acdd24f
Dalvinderjit Singh <66400608+dalvinderjitsingh@users.noreply.github.com>
2024-07-25 02:28:21 +0800

    🚧 Add text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Bricolage_Grotesque, Inter } from "next/font/google";
 import "@/styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Bricolage_Grotesque({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
